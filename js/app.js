d0965f1bb95e1cc54c0ebb6175c36ef50a667042
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-18 00:26:34 +0200

    Refactor function userAnswerPromptNumber

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //-------------------------Variabili------------------------------
 const numberCasual = randomNumberArray(1, 100, 5);
-
+let numberWin = [];
 //-------------------------DOMElement-----------------------------
 const numberDOMElement = document.getElementById("number");
 const timerDOMElement = document.getElementById("timer");
@@ -15,8 +15,13 @@ decreasingTimer(timerDOMElement, 30);
 deleteDOMElementContent(numberDOMElement);
 
 // - Inserire 5 numeti tremmite prompt
-const answer = userAnswerPromptNumber(5, 33000);
-console.log(answer);
+const idSetTimeOut = setTimeout(function(){
+    console.log(numberCasual);
+    const answer = userAnswerPromptNumber(5);
+    numberWin = compareArrays(numberCasual, answer);
+    console.log(numberWin);
+}, 32000)
+
 // - Controllare se i numeri inseriti sono correti e capire quale sono stati indovinati
 
 //-------------------------FUNZIONI-----------------------------
@@ -63,14 +68,12 @@ function decreasingTimer(DOMElement, counterStart){
 }
 
 // Funzione per ricevere risposta di utente
-function userAnswerPromptNumber(numberPrompt, timeout){
+function userAnswerPromptNumber(numberPrompt){
     const array = [];
-    setTimeout(function(){
-        for (let i = 0; i < numberPrompt; i++){
-            const answerPrompt = parseInt(prompt(`Inserisci il numero ${i + 1}`)); 
-            array.push(answerPrompt);
-        }
-    }, timeout)
+    for (let i = 0; i < numberPrompt; i++){
+        const answerPrompt = parseInt(prompt(`Inserisci il numero ${i + 1}`)); 
+        array.push(answerPrompt);
+    }
     return array;
 }
 
@@ -78,7 +81,7 @@ function userAnswerPromptNumber(numberPrompt, timeout){
 function compareArrays(array, arrayToCompare){
     const arrayNumberWin = []
     for(let i = 0; i < arrayToCompare.length; i++){
-        if(array.contains(arrayToCompare[i])){
+        if(array.includes(arrayToCompare[i])){
             arrayNumberWin.push(arrayToCompare[i]);
         }
     }
