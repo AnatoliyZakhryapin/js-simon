8a9eecd4a1ee676316493c694d5c66fa9723f745
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-17 23:18:13 +0200

    ADD function decreasingTimer+ print result in page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
-// - Visualizzare in pagina 5 numeri casuali (anche tramite alert).
+//-------------------------Variabili------------------------------
 const numberCasual = randomNumberArray(1, 100, 5);
 
-//     - Creare DOMElement per inserire i numeri
+//-------------------------DOMElement-----------------------------
 const numberDOMElement = document.getElementById("number");
+const timerDOMElement = document.getElementById("timer");
+
+// - Inseriamo i valore di numberCasual in html
 numberDOMElement.innerHTML = numberCasual;
 
 // - Creare il timer di 30 secondi
-deleteDOMElementContent(numberDOMElement);
 
+deleteDOMElementContent(numberDOMElement);
+decreasingTimer(timerDOMElement, 30);
 // - Cancellare i dati dopo 30 secondi
 // - Inserire 5 numeti tremmite prompt
 // - Controllare se i numeri inseriti sono correti e capire quale sono stati indovinati
@@ -35,11 +39,24 @@ function randomNumberArray(min, max, n){
 //     DOMElement.innerHTML = "";
 // }
 // setTimeout(deleteDOMElementContent(numberDOMElement), 5000)
-
 function deleteDOMElementContent(DOMElement){
     setTimeout(() => {
         DOMElement.innerHTML = "";
     }, 30000)
+}
+
+// Funzione decreasingTimer
+function decreasingTimer(DOMElement, counterStart){
+    let counter = counterStart;
+    const idecreasingTimer = setInterval(function(){
+        counter--;
+        console.log(counter)
+        DOMElement.innerHTML = counter;
+        if(counter === 0){
+            clearInterval(idecreasingTimer);
+            DOMElement.innerHTML = "Tempo scaduto";
+        }
+    }, 1000)
 }
 
 
